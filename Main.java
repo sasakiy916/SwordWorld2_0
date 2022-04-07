d4b057dda7a2161a05b593579b48ec7d2d633bcb
sasakiy916 <sasaki3y98521j@gmail.com>
2022-04-07 23:29:18 +0900

    aaa

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main{
 	public static void main(String[] args){
+		int a = 1;
 		MainMenu menu = new MainMenu("ソードーワールド再現");
 		Character h = new Human();
 		Character m = new Kobold();
